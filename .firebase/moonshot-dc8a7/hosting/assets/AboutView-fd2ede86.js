ca49034e59d387cc8d98c5e7e2535d2fda9a97e2
AiMingQi <cafevalentine@gmail.com>
2023-03-23 14:45:17 -0600

    add Firebase

---
+++ .firebase/moonshot-dc8a7/hosting/assets/AboutView-fd2ede86.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s,a as t}from"./index-c60a262d.js";const _={},c={class:"about"},n=t("h1",null,"This is an about page",-1),a=[n];function i(r,u){return o(),s("div",c,a)}const d=e(_,[["render",i],["__file","C:/Users/AiMingQi/Projects/MoonShot/src/views/AboutView.vue"]]);export{d as default};
